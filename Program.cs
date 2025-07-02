54ef3c63ee90d95591732e7a7d2616c2bf956bd0
HardipZapadiya <hardipzapadiya18@gmail.com>
2025-07-02 11:20:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hardipzapadiya_23SOECE11020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!"); 
+        }
+    }
+}
